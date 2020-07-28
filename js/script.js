d76fea06314d95e57601e05d789a496e470b03df
caterina <catemarian1806@gmail.com>
2020-07-28 19:15:45 +0200

    serie tv

--- js/script.js
+++ js/script.js
@@ -71,29 +71,60 @@ function movieResults(search) {
   });
 }
 // tv series
+function tvserieData(serie,target,compiled) {
+  var seriedata = {
+    "name":serie["name"],
+    "original_name":serie["original_name"],
+    "original_language":serie["original_language"],
+    "origin_country":serie["origin_country"],
+    "vote":serie["vote_average"]/2,
+    "star1":"",
+    "star2":"",
+    "star3":"",
+    "star4":"",
+    "star5":"",
+    "flag": ""
+    }
+    var vote5 = Math.ceil(seriedata["vote"]);
+    console.log(vote5);
+    for (var i = 1; i <= vote5; i++) {
+      seriedata["star"+i] = "gialla";
+    }
+    if (seriedata["origin_country"] == "US") {
+      seriedata["flag"] = "img/USAflag.gif";
+    }
+    else if (seriedata["origin_country"] == "IT") {
+      seriedata["flag"] = "img/italflag.png";
+    }
+    else if (seriedata["origin_country"] == "JP") {
+      serieata["flag"] = "img/jpflag.svg";
+    }
+  // console.log(moviedata);
+  var serieHtml = compiled(seriedata);
+  target.append(serieHtml);
+}
 function tvseriesResults(search) {
   $.ajax({
-    url:"https://api.themoviedb.org/3/search/movie",
+    url:"https://api.themoviedb.org/3/search/tv",
     method:"GET",
     data: {
       "api_key":"044d0c2aa05cc2030718c5e50899f07d",
       "query": search
     },
     success: function(data,state){
-      var moviesnum = data["total_results"];
-      var movies = data["results"];
+      var seriesnum = data["total_results"];
+      var series = data["results"];
 
       // console.log(moviesnum);
-      var template = $("#movie-template").html();
+      var template = $("#serietv-template").html();
       var compiled = Handlebars.compile(template);
-      var target = $("#resultsmovie");
+      var target = $("#resultstvs");
       target.text("");
 
-      for (var i = 0; i < movies.length; i++) {
-        var movie = movies[i];
-        movieData(movie,target,compiled);
-        // var movieHtml = compiled(movie);
-        // target.append(movieHtml);
+      for (var i = 0; i < series.length; i++) {
+        var serie = series[i];
+        tvserieData(serie,target,compiled);
+
       }
     },
     error: function (request,state,error) {
@@ -105,7 +136,7 @@ function tvseriesResults(search) {
 }
 // button
 function search() {
-  var search = $("#earch").val();
+  var search = $("#search").val();
   console.log(search);
   // var prova = "ritorno al futuro";
   movieResults(search);
